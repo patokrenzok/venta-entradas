036e41b38baac59c10466700f8bbde74d4552555
Agustin Zamar <agustinzamar33@gmail.com>
2022-11-09 21:12:37 -0300

    lint

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -2,12 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Concerns\HasUuids;
 
 class Ticket extends Model
 {
     use HasFactory, HasUuids;
-
 }
---
+++ resources/js/api/UsersApi.js
@@ -0,0 +1,20 @@
+import axiosIntance from '@/api/apiHelper';
+
+class UsersApi {
+  async get() {
+    const response = await axiosIntance.get('/users');
+    return response.data;
+  }
+
+  async create(data) {
+    const response = await axiosIntance.post('/users', data);
+    return response.data;
+  }
+
+  async getRoles() {
+    const response = await axiosIntance.get('/roles');
+    return response.data;
+  }
+}
+
+export default new UsersApi();
